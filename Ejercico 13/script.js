e5608f4b2378502ad022a4666925c680e7aa7396
MiguelCriollo <116099533+MinusTrece@users.noreply.github.com>
2023-07-05 10:55:02 -0500

    tarea 12

--- Ejercico 13/script.js
+++ Ejercico 13/script.js
@@ -1,11 +1,11 @@
-const notas = [90, 70, 60];
+let notas = [90, 70, 60];
 let sum = 0;
 
 for (let i = 0; i < notas.length; i++) {
   sum += notas[i];
 }
 
-const promedio = sum / notas.length;
+let promedio = sum / notas.length;
 
 if (promedio > 70) {
   console.log("Aprobado");
